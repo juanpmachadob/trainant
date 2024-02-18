47a774cc210795d776788046f4a9b66e62dbd724
juanpmachadob <juanpmachadob@gmail.com>
2024-02-18 02:20:20 -0500

    feat: introduce valuesChanged state in useForm hook to track form modifications

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 /**
  * Custom hook to handle form inputs.
@@ -11,8 +11,10 @@ import { useState } from 'react'
  * @returns {Array[]} - Array with the form values, and the functions to handle the form.
  */
 const useForm = (initialState = {}) => {
+  const initialStateRef = useRef(initialState) // Keep track of the initial state
   const [formValues, setFormValues] = useState(initialState)
   const [formErrors, setFormErrors] = useState({})
+  const [valuesChanged, setValuesChanged] = useState(false)
 
   /**
    * Function to reset the form to its initial state.
@@ -20,7 +22,21 @@ const useForm = (initialState = {}) => {
    */
   const reset = (newFormState = initialState) => {
     setFormValues(newFormState)
+    initialStateRef.current = newFormState
   }
+
+  /**
+   * Hook useEffect to check if the form values have changed.
+   */
+  useEffect(() => {
+    // Check if the form values are different from the initial state
+    const valuesChanged = Object.keys(initialStateRef.current).some(
+      (key) => initialStateRef.current[key] !== formValues[key]
+    )
+
+    // Update modified state
+    setValuesChanged(valuesChanged)
+  }, [formValues, initialStateRef])
 
   /**
    * Function to handle input changes.
@@ -39,6 +55,7 @@ const useForm = (initialState = {}) => {
   return {
     formValues,
     setFormValues,
+    valuesChanged,
     handleInputChange,
     formErrors,
     setFormErrors,
