f42f42d98b1ff983e98350d2988c4e95fc20119b
juanpmachadob <juanpmachadob@gmail.com>
2024-03-03 21:22:14 -0500

    feat: implement persistentLocalCache with Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore, persistentLocalCache } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,4 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({})
+})
