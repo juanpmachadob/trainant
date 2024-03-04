03ba3b3321f9d5e3e4879158904fbd21c9a74ab3
juanpmachadob <juanpmachadob@gmail.com>
2024-03-03 21:22:50 -0500

    feat: add service worker for PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { registerSW } from 'virtual:pwa-register'
 import App from '@/App.jsx'
 import store from '@/store'
+import { showAlert } from './utils/alerts'
 import '@/index.css'
 import '@/../node_modules/react-grid-layout/css/styles.css'
+
+const updateSW = registerSW({
+  onNeedRefresh () {
+    showAlert({
+      title: 'New version available!',
+      icon: 'question',
+      confirmButtonText: 'Reload',
+      callback: () => updateSW(true)
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
